d2f54846c1876a24c280d70d7e82ac82212d8f05
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2021-11-08 16:05:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,6 @@ include_once "MVC/db.php";
 // Controleur
 include_once "MVC/action.php";
 
-//Page location
-include_once "MVC/location.php";
-
-//Page transaction
-include_once "MVC/transaction.php";
-
-//Page architecture
-include_once "MVC/architecture.php";
-
 // Vue
 include_once "MVC/view.php";
 
