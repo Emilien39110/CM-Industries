8f3be4735e55233e27c295979220e38177ca5497
Khmarigou <luca.plc@outlook.fr>
2021-11-08 14:44:13 +0100

    Update view.php

--- view.php
+++ view.php
@@ -4,7 +4,22 @@
 	<meta charset="utf-8">
 	<title>CMIndustries</title>
 </head>
-<body>
 
+<header>
+	<ce>Votre Agence immobilière au plus près de chez vous</ce>
+    <nav>
+        <ul>
+            <li><a href="./Architecture.php">Architecture</a></li>
+            <li><a href="./location.php">Location</a></li>
+            <li><a href="./transaction.php">Transaction</a></li>
+            <li><a href="./contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<body>
+	
 </body>
+<footer>
+	
+</footer>
 </html>
