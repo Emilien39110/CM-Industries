09ac755bfe877b4d4885128462d26d28c161ecbd
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2021-11-15 14:25:34 +0100

    Update modele.php

--- MVC/modele.php
+++ MVC/modele.php
@@ -3,11 +3,11 @@
 
 function LoadLocations () {
 	global $c;
-	$sql = "select * from location/sell";
+	$sql = "SELECT * FROM 'Location/sell'";
 	$result = mysqli_query ($c, $sql);
 	$list = [];
 	while ($row = mysqli_fetch_assoc($result))
-		$list[] = $row; // Ajouter dans la liste
+		$list[] = $row;
 	return $list;
 }
 
@@ -15,11 +15,11 @@ function DisplayDonnees ($list) {
 	foreach ($list as $key => $value) {
 		echo "<li>";
 		echo "<h2>".$value["name"]."</h2>";
-		echo "<p>Localisation : ".$value["location"]."</p>";
-		echo "<p>Prix : ".$value["price"]."</p>";
+		echo "<p>Localisation : ".$value["localisation"]."</p>";
+		echo "<p>Prix : ".$value["description"]."</p>";
 		echo "<p>".$value["price"]."</p>";
 		echo "<p>Consommation energie : ".$value["energy"]."</p>";
-		echo "<p>GreenHouse : ".$value["greenhouseEff"]."</p>";
+		echo "<p>GreenHouse : ".$value["greenhouseg"]."</p>";
 		echo "</li>";
 	}
 }
