cade8e12e53e7d195477eec290bc9c1bbcfdf546
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2021-11-15 17:11:37 +0100

    grosse modif

--- MVC/ajouter.php
+++ MVC/ajouter.php
@@ -1,9 +1,10 @@
 <?php
 
-$c = mysqli_connect("localhost", "l2", "L2", "CMIndustries");
-mysqli_set_charset($c, "utf8");	
+	$c = mysqli_connect("localhost", "l2", "L2", "CMIndustries");
+	mysqli_set_charset($c, "utf8");	
 
 	$sql= "INSERT INTO `Location/sell`(`name`, `localisation`, `description`, `price`, `energy`, `greenhouseg`, `location`) VALUES ('".$_POST['nom']."', '".$_POST['localisation']."', '".$_POST['description']."', ".$_POST['prix']." , '".$_POST['energie']."' , '".$_POST['effetserre']."', '".$_POST['location']."')";
+
 	$result=mysqli_query($c,$sql);
-	header("location: ../../PAGES/location.php");
-	?>
+	header("location: ./PAGES/location.php");
+?>
