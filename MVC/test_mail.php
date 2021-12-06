ca5dcf523d440afd5df1dc68465794f52cdb5b42
MaxBuster <92114663+MaxBuster380@users.noreply.github.com>
2021-12-06 15:58:16 +0100

    Update test_mail.php

--- MVC/test_mail.php
+++ MVC/test_mail.php
@@ -2,8 +2,8 @@
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 require_once('./MVC/PHPMailer/src/PHPMailer.php');
-require_once('./PHPMailer/src/SMTP.php');
-require_once('./PHPMailer/src/Exception.php');
+require_once('./MVC/PHPMailer/src/SMTP.php');
+require_once('./MVC/PHPMailer/src/Exception.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
