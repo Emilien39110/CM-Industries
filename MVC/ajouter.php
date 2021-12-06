23c3f67df3d18d00d60a78981e4229215656acfe
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2021-12-06 15:34:58 +0100

    C'est bon

--- MVC/ajouter.php
+++ MVC/ajouter.php
@@ -1,4 +1,5 @@
 <?php
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	mkdir("../IMAGES/".$_POST['nom']);
 	$uploaddir = '../IMAGES/'.$_POST['nom'].'/';
 	$uploadfile1 = $uploaddir . basename($_FILES['fichierimage1']['name']);
@@ -8,12 +9,16 @@
 	$uploadfile3 = $uploaddir . basename($_FILES['fichierimage3']['name']);
 	move_uploaded_file($_FILES['fichierimage3']['tmp_name'], $uploadfile3);
 
+	$estvendu = 0;
 
 	$c = mysqli_connect("localhost", "l2", "L2", "CMIndustries");
 	mysqli_set_charset($c, "utf8");	
-	$sql= "INSERT INTO location_vente(name, localisation, description, price, energy, greenhouseg, location, type, surface) VALUES ('".$_POST['nom']."', '".$_POST['localisation']."', '".$_POST['description']."', ".$_POST['prix']." , '".$_POST['energie']."' , '".$_POST['effetserre']."', '".$_POST['location']."','".$_POST['type']."',".$_POST['surface'].")";
+	$sql= "INSERT INTO location_vente(name, localisation, description, price, energy, greenhouseg, location, type, surface, vendu) VALUES ('".$_POST['nom']."', '".$_POST['localisation']."', '".$_POST['description']."', ".$_POST['prix']." , '".$_POST['energie']."' , '".$_POST['effetserre']."', '".$_POST['location']."','".$_POST['type']."',".$_POST['surface'].",".$estvendu.")";
 	$result=mysqli_query($c,$sql);
 	
+
+
+
 	$adresse = $_SERVER['REQUEST_URI'];
 	$script_courant = basename($_SERVER["SCRIPT_FILENAME"], '.php') . ".php";
 	$morceau_a_remplacer = "MVC/" . $script_courant;
@@ -25,4 +30,5 @@
 	}
 	$resultat = str_replace($morceau_a_remplacer, $a_remplacer_par, $adresse);
 	header("location:" . $resultat);
+
 ?>
